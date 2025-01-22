7d18d1d5b7226132d253b1024843ce7ae1a879d1
dhiraj <dhirajc1337@gmail.com>
2025-01-22 16:20:53 +0530

    api calls testing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => {
-    res.send("hellow from the qserversssss!") // request handler functioon
+
+
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Dhiraj", lastName: "Channe" })
 })
+
+app.post("/user", (req, res) => {
+
+    res.send("data successfully saved to database!")
+})
+app.use("/test", (req, res) => {
+    res.send("server testing activated")
+})
+
 app.listen(3000, () => {
     console.log("server running on port 3000")
 });
