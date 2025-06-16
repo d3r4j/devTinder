6bfee234e0060cfffe017452116e77266b7f8007
dhiraj <dhirajc1337@gmail.com>
2025-06-16 20:07:55 +0530

    feed api done

--- src/app.js
+++ src/app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database")
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
+const userModel = require("./models/user");
+const cookies = require("cookie-parser");
 
 
-app.use(express.json())
+app.use(express.json());
+// create new user  / signup API 
+app.use(cookies()) // midddleware to read cookies
 
-app.post("/signup", async (req, res) => {
-    // console.log(req.body)
-    // const userObj = {
-    //     firstName: "alex",
-    //     lastName: "thompson",
-    //     emailId: "alex@gmail.com",
-    //     password: "alexj123 ",
-    //     age: "26",
-    // }
 
-    try {
-        // creating a new instance of a user model 
-        const user = new User(req.body)
-        await user.save();
-        res.send("User addded successfully in database")
-    }
-    catch (err) {
-        res.status(400).send("error saving the user" + err.message)
-    }
+const authRouter = require("./routes/auth")
+const profileRouter = require("./routes/profile")
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
-})
+app.use("/", authRouter)
+app.use("/", profileRouter)
+app.use("/", requestRouter)
+app.use("/", userRouter)
 
 
 
@@ -42,6 +35,120 @@ connectDB()
 
     })
     .catch(err => {
-
+        res.status(400).send("ERROR >>  " + err.message)
     })
 
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// find all users  having email id
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("user not found")
+        } else {
+
+            res.send(users)
+        }
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
+})
+
+// finding user having specific email
+app.get("/userEmail", async (req, res) => {
+    try {
+        const email = req.body.emailId;
+        const user = await userModel.findOne({ emailId: email });
+        res.send(user);
+
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
+})
+// find user by user id
+app.get("/userId", async (req, res) => {
+    const id = req.body._id;
+    try {
+        console.log(id);
+        const user = await userModel.findById(id)
+        res.send(user);
+
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
+})
+
+// feed api to GET all users from DB
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({}); //empty obj means all users are selected
+        res.send(user)
+
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
+
+})
+
+// delete a user by user id
+app.delete("/user", async (req, res) => {
+    const deleteID = req.body.userId;
+    try {
+        // console.log(deleteID)
+        const user = await userModel.findOneAndDelete(deleteID);
+        res.send("user deleted successfully")
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
+
+})
+
+// update data of user
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;   // doing _id was giving error , see why?
+
+    const data = req.body; //if its not present in schema it will not be added to db. as userID was passed but not updated in db.
+
+
+    try {
+
+        const ALLOWED_UPDATES = [
+            "photoUrl", "age", "gender", "about", "password", "skills",
+        ] // things we allow for user to update here.
+
+        const isUpdateAllowed = Object.keys(data).every(key => ALLOWED_UPDATES.includes(key)) //loops through all keys to check of it has our allowed updates field to edit
+
+        if (!isUpdateAllowed) {
+            throw new Error("cannot update this section")
+        }
+
+        if (data?.skills.length > 10) {
+            throw new Error("skills cannnot be more than 10") // validation for skills length
+        }
+
+        // console.log("id", userId, data)
+        await userModel.findByIdAndUpdate({ _id: userId }, data, {
+            retrunDocument: "after",
+            runValidators: true,
+        })
+        console.log(User);
+        res.send("user updated successfully");
+
+    }
+    catch (err) {
+        res.status(400).send("update failed:" + err.message)
+    }
+
+})
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,28 +1,37 @@
-const adminAuth = (req, res, next) => {
-    console.log("admin auth is getting checked")
-    const token = "xyz";
-    const isAdminAuth = token === "xyz";
-    if (!isAdminAuth) {
-        res.status(401).send("unauthorized access, request denied")
-    }
-    else {
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+const userAuth = async (req, res, next) => {
+    // read the token from the request cookies
+    try {
+        const { token } = req.cookies;
+        if (!token) {
+            throw new Error("invalid token")
+        }
+        const decodedObj = await jwt.verify(token, "secertKey@123")  // line 79 of app.js
+        const { _id } = decodedObj;
+
+        const user = await User.findById(_id)
+        if (!user) {
+            throw new Error("user not found")
+        }
+        // validate the token 
+        // find  the username
+        req.user = user; // user found in line 13 will be attached with the request  
         next();
     }
+
+    catch (err) {
+        res.status(400).send("ERROR:" + err.message)
+    }
+
+
+
+
 }
 
-const userAuth = (req, res, next) => {
-    console.log("user auth is getting checked")
-    const token = "xyza";
-    const isAdminAuth = token === "xyz";
-    if (!isAdminAuth) {
-        res.status(401).send("unauthorized access, request denied")
-    }
-    else {
-        next();
-    }
-}
 
 
 module.exports = {
-    adminAuth, userAuth,
+    userAuth
 }
